5c646a311f04b52cd6077d98d22054d289374abc
David Tuite <dtuite@gmail.com>
2020-07-02 16:24:55 +0100

    Support turning on SSL via env vars

--- src/run.ts
+++ src/run.ts
@@ -15,10 +15,22 @@
  */
 import { startServer } from '.';
 import logger from './logger';
+import fs from 'fs';
 
 startServer({
   port: process.env.LAS_PORT ? Number(process.env.LAS_PORT) : undefined,
   cors: process.env.LAS_CORS ? Boolean(process.env.LAS_CORS) : undefined,
+  postgresConfig: {
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: {
+      ca: fs.readFileSync(String(process.env.PGPATH_TO_CA)),
+      rejectUnauthorized: true,
+    },
+  },
 }).catch(err => {
   logger.error(err);
   process.exit(1);
